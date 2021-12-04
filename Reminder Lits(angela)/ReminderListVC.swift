bc425793cc62d354753eb6a883f6d0f5f57b1bbd
eng <33827384+eng-ahmedhussien@users.noreply.github.com>
2021-12-04 21:35:29 +0200

    using user default method

--- Reminder Lits(angela)/ReminderListVC.swift
+++ Reminder Lits(angela)/ReminderListVC.swift
@@ -7,13 +7,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ReminderListVC: UITableViewController{
+    
+    var Rlist = ["list 1","list 2","list 3"]
+    let uDefault = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        if let  list = uDefault.array(forKey: "RlistUpdate") as? [String]{
+            Rlist = list
+        }
+        
     }
-
-
+    
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title:"add item", message:"", preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "add", style: .default) { action in
+            if textField.text != ""{
+                self.Rlist.append(textField.text!)
+                self.uDefault.set(self.Rlist, forKey: "RlistUpdate")
+            }
+            self.tableView.reloadData()
+        }
+        //MARK: add textfiled in alert
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "create new item"
+            textField = alertTextField
+        }
+        alert.addAction(okButton)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Rlist.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "tablecell", for: indexPath)
+            cell.textLabel?.text = Rlist[indexPath.row]
+            return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //MARK: animation when deselect row
+        // tableView.deselectRow(at: indexPath,animated: true)
+        
+        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+            //MARK: checkmark row when selected
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        Rlist.remove(at: indexPath.row)
+        tableView.reloadData()
+    }
+   
 }
+
+
 
